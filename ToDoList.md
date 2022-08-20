27043612c99717040373e1f6e0ff4dcfee8aa3c0
Jake Sinclair <100993873+JakeSincl@users.noreply.github.com>
2022-08-20 13:58:07 +0000

    Initial commit

---
+++ ToDoList.md
@@ -0,0 +1,21 @@
+# To Do List
+
+## Done
+* ~~Add login button to home page~~
+* ~~Change how the pages are routed from page to name of function~~
+* ~~Create Support page~~
+* ~~Create Login Page~~
+* ~~The next session thing seems to be busted.~~
+* ~~Create Logged out page~~
+* ~~Add passwords to database~~
+* ~~Validate Password to Username~~
+
+## Fix
+
+
+## Create New
+* Continue working on dasboard
+* Different levels of account functionality
+
+## Resources
+* https://www.youtube.com/watch?v=dam0GPOAvVI
---
+++ website/models.py
@@ -0,0 +1,30 @@
+from enum import unique
+from time import timezone
+from . import db
+from flask_login import UserMixin
+from sqlalchemy.sql import func
+
+class Note(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    data = db.Column(db.String(10000), unique=True)
+    date = db.Column(db.DateTime(timezone=True), default=func.now())
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(150))
+    author = db.Column(db.String(150))
+    description = db.Column(db.String(1000))
+    publish_year = db.Column(db.Integer)
+    currently_loaned = db.Column(db.Boolean())
+    loaned_to = db.Column(db.Integer, db.ForeignKey("user.id"))
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(150), unique=True)
+    password = db.Column(db.String(150))
+    first_name = db.Column(db.String(150))
+    access_level = db.Column(db.Integer)
+    notes = db.relationship("Note")
+    books = db.relationship("Book")
